8d63c6df819eb3490a29606f730f98164f80e4b8
dcasiano <82372508+dcasiano@users.noreply.github.com>
2023-11-12 00:50:39 +0100

    proyecto unity

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+
+    private Text daysCounter;
+    private int currentDay;
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(this.gameObject);
+    }
+   
+    void Start()
+    {
+        currentDay = 1;
+        UpdateDaysCounter();
+    }
+
+    void Update()
+    {
+
+    }
+    public static GameManager GetInstance()
+    {
+        return instance;
+    }
+    public void OnNextTurnButtonPressed()
+    {
+        currentDay++;
+        UpdateDaysCounter();
+    }
+    private void UpdateDaysCounter()
+    {
+        daysCounter = GameObject.Find("DaysCounter").GetComponent<Text>();
+        if (daysCounter != null) daysCounter.text = "Día " + currentDay;
+    }
+}
---
+++ Assets/Scripts/NextTurnButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NextTurnButton : MonoBehaviour
+{
+    private GameManager gm;
+   
+    void Start()
+    {
+        gm = GameManager.GetInstance();
+        Button btn = GetComponent<Button>();
+        btn.onClick.AddListener(gm.OnNextTurnButtonPressed);
+    }
+
+}
